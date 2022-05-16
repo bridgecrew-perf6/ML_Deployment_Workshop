68882ae2df289861460fa57a8ab954ce98548a2b
mohithsairamreddy <mohithsairamreddy@gmail.com>
2022-05-16 18:29:03 +0530

    App.py using stramlit

---
+++ App.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.title("Title")
+st.header("Header")
+st.subheader("Subheader")
+st.text("Text")
+st.info("Info")
+st.warning("Warning")
+st.error("Error")
+st.success("Success")
